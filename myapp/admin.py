e60599258e9ef762ed55a8c4f8385943823e840b
sarveshkrg <46168717+sarveshkrg@users.noreply.github.com>
2018-12-29 19:15:56 +0530

    .

---
+++ myapp/admin.py
@@ -0,0 +1,2 @@
+from django.contrib import admin
+# I will register my model when i will create
---
+++ users/forms.py
@@ -0,0 +1,28 @@
+"""
+This forms.py has been created for adding extra fields
+"""
+
+from django import forms
+from django.contrib.auth.models import User # built in model in django
+from django.contrib.auth.forms import UserCreationForm # since UserCreationForm has only default fields [username, password1, password2] so we create UserRegisterForm and inherit UserCreationForm to add email
+from . models import Profile
+
+class UserRegisterForm(UserCreationForm):
+    email =  forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email =  forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = ['image']
